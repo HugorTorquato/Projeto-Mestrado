93a6cc6b4c2145354c42835de378d36f183851e7
Hugo Torquato <hugortorquato@gmail.com>
2021-05-16 12:07:09 -0300

    Pequenas alterações

--- Python/DB_Rede.py
+++ Python/DB_Rede.py
@@ -1,13 +1,28 @@
 import sqlalchemy as sql
+import pandas as pd
 
 
 def sqlalchemyVersion():
 
-    engine = sql.create_engine('mssql+pymssql://sa:sa123@localhost:3306/DB_Rede3')
+    engine = sql.create_engine('mssql+pymssql://sa:sa123@localhost:3306/DB_Rede_3', echo=True)
     print(engine)
+    df = pd.read_sql('SELECT TOP (1000) [Nome] ,[idade] FROM [DB_Rede3].[dbo].[Table_1]', engine)
+    print(df)
+    #Create_Tables(engine)
 
 
-## Criar engine com o SQL Server -> engine = create_engine('sqlite:///:memory:', echo=True)
+def Create_Tables(engine):
+
+    metadata = sql.MetaData()
+    users = sql.Table('addresses', metadata,
+                   sql.Column('id', sql.Integer, primary_key=True),
+                   sql.Column('user_id', None, sql.ForeignKey('users.id')),
+                   sql.Column('email_address', sql.String, nullable=False))
+    metadata.create_all(engine)
+
+    print('hugo')
+
+
 ## Criar chart com a dinamica do banco e as possiveis conexoes que podem ser feitas entra as tabelas
 ## Criar as tabelas
 
